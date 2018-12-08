00ca14e08c1835cbfd8b333ee20539c17b38a392
swaraj <jena.swaraj@gmail.com>
2018-12-08 17:24:27 +0530

    multiple select filter

--- routes/db_retrive.js
+++ routes/db_retrive.js
@@ -47,7 +47,7 @@ router.post('/filter_data',async (req,res) => {
     let lm_db_columns =  result.recordset.map(column => "lm."+column.COLUMN_NAME)
                         .filter(column => !(column.indexOf("_ID")>=0)) 
 
-    let lm_filters = filters.filter(fl => (fl.selected!=null && lm_columns.indexOf(fl.Attribute_name)>=0))
+    let lm_filters = filters.filter(fl => (fl.selected && lm_columns.indexOf(fl.Attribute_name)>=0))
     lm_filters = lm_filters.map(filter => {
         let qry= "AND (";
         for (let opt of filter.selected){
